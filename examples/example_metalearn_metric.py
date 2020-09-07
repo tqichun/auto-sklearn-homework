d0b2d32a540f4e978963dcd90cb932d8750da60e
Hyper-Flow <tqichun@gmial.com>
2020-09-07 21:55:02 +0800

    update

--- examples/example_metalearn_metric.py
+++ examples/example_metalearn_metric.py
@@ -9,15 +9,40 @@ The following example shows how to fit a simple classification model with
 """
 import os
 
+import openml
+import pandas as pd
 import sklearn.metrics
+from sklearn.preprocessing import LabelEncoder
 
 import autosklearn.classification
+
 
 ############################################################################
 # Data Loading
 # ============
+def load_dataset(dataset_id):
+    dataset = openml.datasets.get_dataset(dataset_id)
+    tasks_a = openml.tasks.list_tasks(task_type_id=1, status='active')
+    tasks_a = pd.DataFrame.from_dict(tasks_a, orient="index")[['tid', 'did', 'name', 'target_feature']]
+    target = tasks_a.query(f'did == {dataset_id}')['target_feature'].tolist()[0]
+    df, y, cat, _ = dataset.get_data(target=target)
+    cat_cols = df.select_dtypes(['category']).columns
+    df[cat_cols] = df[cat_cols].astype(object)
+    obj_cols = df.select_dtypes(['object']).columns
+    for col in obj_cols:
+        mask = ~pd.isna(df[col])
+        df[col][mask] = LabelEncoder().fit_transform(df[col][mask])
+    y = LabelEncoder().fit_transform(y)
+    df = df.astype(float)
+    return df.values, y, cat
 
-X, y = sklearn.datasets.load_breast_cancer(return_X_y=True)
+
+dataset_id = 2
+
+X, y, cat = load_dataset(dataset_id)
+
+feat_type = ["Categorical" if c else "Numerical" for c in cat]
+
 X_train, X_test, y_train, y_test = \
     sklearn.model_selection.train_test_split(X, y, random_state=1)
 
@@ -31,7 +56,7 @@ automl = autosklearn.classification.AutoSklearnClassifier(
     tmp_folder='/tmp/autosklearn_classification_example_tmp',
     output_folder='/tmp/autosklearn_classification_example_out',
 )
-automl.fit(X_train, y_train, dataset_name='2')
+automl.fit(X_train, y_train, dataset_name=str(dataset_id), feat_type=feat_type)
 
 ############################################################################
 # Print the final ensemble constructed by auto-sklearn
@@ -45,3 +70,5 @@ print(automl.show_models())
 
 predictions = automl.predict(X_test)
 print("acc score:", sklearn.metrics.accuracy_score(y_test, predictions))
+print("recall score:", sklearn.metrics.recall_score(y_test, predictions))
+print("f1 score:", sklearn.metrics.f1_score(y_test, predictions))
